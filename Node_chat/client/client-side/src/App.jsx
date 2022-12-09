a2b7251e73c52395735ce3bf562adc153e38c70a
JohnLucid1 <SobolII21@st.ithub.ru>
2022-12-09 04:27:31 +0300

    cool chat

--- Node_chat/client/client-side/src/App.jsx
+++ Node_chat/client/client-side/src/App.jsx
@@ -1,79 +1,44 @@
-import { useEffect } from 'react'
 import { useState } from 'react'
 import io from "socket.io-client"
+import Chat from './Chat'
+import "./App.css"
 
-const socket = io.connect("http://localhost:4000")
+const socket = io.connect("http://localhost:3001")
 
 function App() {
-  const [state, setState] = useState({ message: "", name: "" })
-  const [chat, setChat] = useState([])
-
-  useEffect(() => {
-    socket.on('message', ({ name, message }) => {
-      setChat([...chat, { name, message }])
-    })
-  })
-
-  const onTextChange = e => {
-    setState({ ...state, [e.target.name]: e.target.value })
+  const [username, setUsername] = useState("")
+  const [room, setRoom] = useState("")
+  const [showChat, setShowChat] = useState(false)
+  
+  
+  const checkRoom = (e) => {
+    console.log(e);
+    e == "" ? setRoom("1") : setRoom(e)
   }
 
-  const onMessageSubmit = e => {
-    e.preventDefault()
-    const { name, message } = state
-    socket.emit('message', { name, message })
-    setState({ message: "", name })
+  
+  const joinRoom = () => {
+    // if(username !== "" && room !== ""){
+    if(username !== ""){
+      socket.emit("join_room", room)
+      setShowChat(true)
+    }
   }
-
-
-  const renderChat = () => {
-    return chat.map(({name, message}, index) => (
-      <div key={index} className="Messages">
-        <h3 className='MainMessages'>
-          {name}: <span>{message}</span>
-        </h3>
-      </div>
-    ))
-  }
+  
   return (
-      <div className="MainApp">
-
-        <div className="littleApp">
-          <form onSubmit={onMessageSubmit} >
-
-            <h1 style={{
-              display: "flex",
-              justifyContent: "center"
-            }}>Messanger</h1>
-
-            <div className="name_field" style={{ display: "flex", justifyContent: "center" }}>
-              <input required className="input_field" name="name" placeholder="Name" onChange={e => onTextChange(e)} value={state.name} label='Name' />
-            </div>
-
-            <div className="message_field" style={{ display: "flex", justifyContent: 'center' }}>
-              <input maxLength={30} required className="input_field" name="message" placeholder='Message' onChange={e => onTextChange(e)} value={state.message} label='Message' />
-            </div>
-
-            <div className="MainButton">
-              <button className="SendButton">ENTER</button>
-            </div>
-
-          </form>
-
-          <div className="render_chat" style={{
-            display: "flex",
-            justifyContent: "center",
-          }}>
-            <div className="messages">
-              <h1 style={{
-                display: "flex",
-                justifyContent: "center"
-              }}>Chat Logs</h1>
-              {renderChat()}
-            </div>
-          </div>
-        </div>
+    <div className='App'>
+    {!showChat ? (
+      <div className='joinChatContainer'>
+        <h3>Онлайн Чат</h3> 
+        <h6>В поле "комната" введите ID комнаты, либо оставьте пустым, чтобы войти в публичную</h6>
+        <input type="text" placeholder='Иван' onChange={(event) => {setUsername(event.target.value)}} />
+        <input type="text" placeholder='Комната' onChange={(event) => {checkRoom(event.target.value)}} />
+        <button onClick={joinRoom}>Войти в чат</button>
       </div>
+    ) : (
+      <Chat socket={socket} username={username} room={room} />
+    )}
+    </div>
   )
 }
 
